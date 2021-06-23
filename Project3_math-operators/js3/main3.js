93a8748e9af3636d92cd6beb6ba31fd316e0314a
MysRAM <l8ureen.ramirez@gmail.com>
2021-06-23 13:42:03 -0700

    commit project3

--- Project3_math-operators/js3/main3.js
+++ Project3_math-operators/js3/main3.js
@@ -1,5 +1,20 @@
+//addition function--
 function addition_function() {
     var addition = 2 + 2;
-    document.getElementById("math").innerHTML = "2 + 2 = " + addition;
+    document.getElementById("add").innerHTML = "2 + 2 = " + addition;
 }
 
+//subtraction function--
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("minus").innerHTML = "5 - 2 = " + subtraction;
+}
+
+// Assign values to x and y
+let x = 10;
+let y = 20;
+
+//add x and y and assign the sum to z
+let z = x + y;
+
+console.log(z)
