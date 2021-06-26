672d3ec4341a147cd62c46232a445a70afbd6e00
MysRAM <l8ureen.ramirez@gmail.com>
2021-06-26 16:50:30 -0700

    6 commit

--- Project6_ternary_operators_constructors/js/main6.js
+++ Project6_ternary_operators_constructors/js/main6.js
@@ -28,7 +28,7 @@ var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = "Erick drives a " + Erik.Vehicle_Color + Erick.Vehicle_Model + Erik.Vehicle_Year;
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + Erik.Vehicle_Year + Erik.Vehicle_Make + Erik.Vehicle_Model;
 }
     
 //new/this
@@ -37,9 +37,25 @@ function Client(Program, OneonOne , PaymentPlan, Race) {
     this.Client_OneonOne = OneonOne;
     this.Clent_PaymentPlan = PaymentPlan;
 }
-var Elisabeth = new Client("InnerSANCTUM", "Yes", "none");
-var Lisa = new Client("Consultation", "Yes", "50%");
-var Emily = new Client("BackDOOR ShadowWORK", "Yes", "25%/wk");
-function myClient() {
-    document.getElementById("New_and_This").innerHTML = "Elisabelth is enrolled in " + Elisabeth.Client_Program + " and requires " + Elsiabeth.Client_OneonONe + " a one on one and her chosen payment plan includes payments broken into " + Elisabeth.Client_PaymentPlan + "increments";
+var Elisabeth = new Client("InnerSANCTUM", "Does", "none");
+var Lisa = new Client("Consultation", "Does", "half up front");
+var Emily = new Client("BackDOOR ShadowWORK", "Does", "quartly payments per week");
+function myNewThis() {
+    document.getElementById("New_and_This").innerHTML = "Elisabeth is enrolled in " + Elisabeth.Client_Program + " and " + Elisabeth.Client_OneonONe + " a one on one and her chosen payment plan includes payments broken into " + Elisabeth.Client_PaymentPlan + "increments";
 }
+
+//identifiers
+var X = 10; //10 is the integer(whole number) literal
+var Y = "Charlie"; //X & Y are identifiers / charlie is string literal
+
+//nested function
+function count_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
+
