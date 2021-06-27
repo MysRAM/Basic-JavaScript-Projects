efd4c736ef2588430197b4623e0154c9b8c89f57
MysRAM <l8ureen.ramirez@gmail.com>
2021-06-27 10:56:31 -0700

    7commit

--- Project7_scope_time_function/js/main7.js
+++ Project7_scope_time_function/js/main7.js
@@ -19,3 +19,15 @@ function LocalFunction_2() {
     console.log(X + 100);
     document.getElementById("Local_2").innerHTML = "X + 100";
 }
+
+//if statements
+if (1 < 2) {
+    document.write("The left number is smaller than the number on the right")
+}
+
+//Date().getHours() method
+function get_Date() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
