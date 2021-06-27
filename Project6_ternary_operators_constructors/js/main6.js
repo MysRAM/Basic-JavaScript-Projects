8fc927d1c5ae3f696d28e7e288501bcebecb2ed6
MysRAM <l8ureen.ramirez@gmail.com>
2021-06-27 08:38:52 -0700

    Update main6.js

--- Project6_ternary_operators_constructors/js/main6.js
+++ Project6_ternary_operators_constructors/js/main6.js
@@ -2,8 +2,8 @@
 document.write(Bigger = (5 > 1) ? "Left number is bigger":"Right number is bigger");
 function RideFunction() {
     var Height, Can_ride;
-    Height = document.getElementById("Height").nodeValue;
-    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height > 52) ? "You are too short" : "You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
@@ -11,7 +11,7 @@ function RideFunction() {
 document.write(Bigger =(5 >1) ? "Left number is bigger":"Right number is bigger");
 function Vote_Function() {
     var Age, Can_vote;
-    Age = document.getElementById("Age").nodeValue;
+    Age = document.getElementById("Age").value;
     Can_vote = (Age > 18) ? "You are too young" : "You are old enough";
     document.getElementById("Vote").innerHTML = Can_vote + " to vote.";
 }
